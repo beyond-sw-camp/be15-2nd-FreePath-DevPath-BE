6a46d8d2ad6647bcc070b51d975b991a4f78968d
splguyjr <splguyjr@naver.com>
2025-04-04 20:04:02 +0900

    first commit

---
+++ src/main/java/com/freepath/devpath/DevpathApplication.java
@@ -0,0 +1,13 @@
+package com.freepath.devpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevpathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freepath/devpath/DevpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freepath.devpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
