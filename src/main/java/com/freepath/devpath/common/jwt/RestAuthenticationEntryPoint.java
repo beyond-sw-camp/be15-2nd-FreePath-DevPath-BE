de8268360c31fe33142e8ac0b67d13821736d035
didiha <hcring@naver.com>
2025-04-06 15:45:01 +0900

    [style #1] RestAuthenticationEntryPoint 주석 내용 수정

--- src/main/java/com/freepath/devpath/common/jwt/RestAuthenticationEntryPoint.java
+++ src/main/java/com/freepath/devpath/common/jwt/RestAuthenticationEntryPoint.java
@@ -17,7 +17,7 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
         response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 404, 인증 실패
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401, 인증 실패
         String jsonResponse = "{\"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\"}";
         response.getWriter().write(jsonResponse);
     }
