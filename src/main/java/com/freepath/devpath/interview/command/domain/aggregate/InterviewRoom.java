10f207f4a2f38a1afa849959c2ee4c8e21f8f65f
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-04-08 16:46:35 +0900

    [feat #10] 인터뷰 컨텍스트 환경 세팅

---
+++ src/main/java/com/freepath/devpath/interview/command/domain/aggregate/InterviewRoom.java
@@ -0,0 +1,29 @@
+package com.freepath.devpath.interview.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class InterviewRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long interviewRoomId;
+
+    private Long userId;
+
+    private String interviewCategory;
+
+    private LocalDateTime interviewRoomCreatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.interviewRoomCreatedAt = LocalDateTime.now();
+    }
+}
