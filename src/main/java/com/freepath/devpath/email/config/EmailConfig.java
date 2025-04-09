e51386713fd698faa1d0469897007eb5202431f6
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-04-09 12:22:36 +0900

    add Exception

--- src/main/java/com/freepath/devpath/email/config/EmailConfig.java
+++ src/main/java/com/freepath/devpath/email/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.freepath.devpath.email.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,6 +9,10 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 @Configuration
 public class EmailConfig {
+
+    @Value("${APP_PASS}")
+    private String appPassword;
+
     @Bean
     public JavaMailSender mailSender() {//JAVA MAILSENDER 인터페이스를 구현한 객체를 빈으로 등록하기 위함.
 
@@ -15,7 +20,7 @@ public class EmailConfig {
         mailSender.setHost("smtp.gmail.com");// 속성을 넣기 시작합니다. 이메일 전송에 사용할 SMTP 서버 호스트를 설정
         mailSender.setPort(587);// 587로 포트를 지정
         mailSender.setUsername("leessjjgg123@gmail.com");//구글계정을 넣습니다.
-        mailSender.setPassword("cgdj ahnp lnbl fctd");//구글 앱 비밀번호를 넣습니다.
+        mailSender.setPassword(appPassword);//구글 앱 비밀번호를 넣습니다.
 
         Properties javaMailProperties = new Properties();//JavaMail의 속성을 설정하기 위해 Properties 객체를 생성
         javaMailProperties.put("mail.transport.protocol", "smtp");//프로토콜로 smtp 사용
--- src/main/java/com/freepath/devpath/email/exception/EmailAuthException.java
+++ src/main/java/com/freepath/devpath/email/exception/EmailAuthException.java
@@ -1,4 +1,16 @@
 package com.freepath.devpath.email.exception;
 
-public class EmailAuthException {
+import com.freepath.devpath.common.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class EmailAuthException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public EmailAuthException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
 }
