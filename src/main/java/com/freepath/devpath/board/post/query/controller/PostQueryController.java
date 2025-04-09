3c95bfadbac6feb8ca73c89ec009be1cc53f9ad8
splguyjr <splguyjr@naver.com>
2025-04-09 17:07:40 +0900

    [chore #40] 사용되지 않는 import 문 제거

--- src/main/java/com/freepath/devpath/board/post/query/controller/PostQueryController.java
+++ src/main/java/com/freepath/devpath/board/post/query/controller/PostQueryController.java
@@ -12,7 +12,6 @@ import com.freepath.devpath.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
