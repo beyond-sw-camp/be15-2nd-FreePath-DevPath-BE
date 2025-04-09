d8ccc0b6fbcf3412d193309cce32650b05c195d3
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-04-09 15:34:36 +0900

    [feat #32] 면접방 상세 조회 기능 구현

--- src/main/java/com/freepath/devpath/interview/query/controller/InterviewQueryController.java
+++ src/main/java/com/freepath/devpath/interview/query/controller/InterviewQueryController.java
@@ -1,6 +1,7 @@
 package com.freepath.devpath.interview.query.controller;
 
 import com.freepath.devpath.common.dto.ApiResponse;
+import com.freepath.devpath.interview.query.dto.InterviewRoomDetailResponse;
 import com.freepath.devpath.interview.query.dto.InterviewRoomDto;
 import com.freepath.devpath.interview.query.service.InterviewQueryService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestController
@@ -28,6 +28,18 @@ public class InterviewQueryController {
 
         Long userId = Long.valueOf(userDetails.getUsername());
         List<InterviewRoomDto> response = interviewQueryService.getInterviewRoomList(userId);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    /* 면접방 정보 및 면접 이력 조회 */
+    @PreAuthorize("hasAuthority('USER')")
+    @GetMapping("/{interviewRoomId}")
+    public ResponseEntity<ApiResponse<InterviewRoomDetailResponse>> getInterviewRoomDetail(
+            @PathVariable Long interviewRoomId,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        InterviewRoomDetailResponse response = interviewQueryService.getInterviewRoomByRoomId(interviewRoomId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
--- src/main/java/com/freepath/devpath/interview/query/service/InterviewQueryService.java
+++ src/main/java/com/freepath/devpath/interview/query/service/InterviewQueryService.java
@@ -1,5 +1,7 @@
 package com.freepath.devpath.interview.query.service;
 
+import com.freepath.devpath.interview.query.dto.InterviewDetailDto;
+import com.freepath.devpath.interview.query.dto.InterviewRoomDetailResponse;
 import com.freepath.devpath.interview.query.dto.InterviewRoomDto;
 import com.freepath.devpath.interview.query.mapper.InterviewMapper;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,27 @@ public class InterviewQueryService {
 
     private final InterviewMapper interviewMapper;
 
+    /* 사용자가 진행한 면접방 목록 조회 */
     @Transactional(readOnly = true)
     public List<InterviewRoomDto> getInterviewRoomList(Long userId) {
         return interviewMapper.selectInterviewRoomListByUserId(userId);
     }
+
+    /* 면접방 정보 및 면접 이력 조회 */
+    @Transactional(readOnly = true)
+    public InterviewRoomDetailResponse getInterviewRoomByRoomId(Long interviewRoomId) {
+
+        InterviewRoomDetailResponse response = new InterviewRoomDetailResponse();
+
+        InterviewRoomDto room = interviewMapper.selectInterviewRoomByRoomId(interviewRoomId);
+        response.setInterviewRoomId(room.getInterviewRoomId());
+        response.setInterviewCategory(room.getInterviewCategory());
+        response.setInterviewRomCreatedAt(room.getInterviewRoomCreatedAt());
+
+        List<InterviewDetailDto> interviews = interviewMapper.selectInterviewListByRoomId(interviewRoomId);
+        response.setInterviewList(interviews);
+
+        return response;
+    }
+
 }
