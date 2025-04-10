85beedd54f38d98c39b8812a16c714cfd983c596
splguyjr <splguyjr@naver.com>
2025-04-10 11:31:13 +0900

    [feat #50] 투표 등록 기능 구현

---
+++ src/main/java/com/freepath/devpath/board/vote/command/dto/request/VoteCreateRequest.java
@@ -0,0 +1,23 @@
+package com.freepath.devpath.board.vote.command.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class VoteCreateRequest {
+
+    @NotBlank(message = "투표 제목은 필수입니다.")
+    private String voteTitle;
+
+    @NotNull(message = "투표 마감일은 필수입니다.")
+    private LocalDateTime voteDuedate;
+
+    @Size(min = 2, message = "투표 항목은 최소 2개 이상이어야 합니다.")
+    private List<String> options;
+}
