ac97f2c6b0f4a78dac0548edcfbe6c5a6967b65d
splguyjr <splguyjr@naver.com>
2025-04-10 11:37:59 +0900

    [refactor #50] 투표 마감일 @Validation 추가
    현재보다 이후의 날짜로만 등록 가능

--- src/main/java/com/freepath/devpath/board/vote/command/dto/request/VoteCreateRequest.java
+++ src/main/java/com/freepath/devpath/board/vote/command/dto/request/VoteCreateRequest.java
@@ -1,5 +1,6 @@
 package com.freepath.devpath.board.vote.command.dto.request;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,6 +17,7 @@ public class VoteCreateRequest {
     private String voteTitle;
 
     @NotNull(message = "투표 마감일은 필수입니다.")
+    @Future(message = "투표 마감일은 현재보다 미래여야 합니다.")
     private LocalDateTime voteDuedate;
 
     @Size(min = 2, message = "투표 항목은 최소 2개 이상이어야 합니다.")
