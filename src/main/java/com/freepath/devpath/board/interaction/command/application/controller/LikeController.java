0107ff22556ee73dd99ce96ef46450512037bfc5
nineeko <gkxm1013@naver.com>
2025-04-11 17:10:41 +0900

    [feature #46] 좋아요, 북마크 관련 기능 구현

--- src/main/java/com/freepath/devpath/board/interaction/command/application/controller/LikeController.java
+++ src/main/java/com/freepath/devpath/board/interaction/command/application/controller/LikeController.java
@@ -9,7 +9,6 @@ import com.freepath.devpath.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
