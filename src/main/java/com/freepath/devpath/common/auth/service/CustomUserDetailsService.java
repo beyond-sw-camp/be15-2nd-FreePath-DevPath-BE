978588094f6460b0d51f4d4c41134a471c1093d8
splguyjr <splguyjr@naver.com>
2025-04-12 21:16:48 +0900

    [refactor #93] 게시글, 댓글 신고 로직 수정

--- src/main/java/com/freepath/devpath/common/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/freepath/devpath/common/auth/service/CustomUserDetailsService.java
@@ -19,6 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        System.out.println("테스트");
         User user = userCommandRepository.findById(Long.valueOf(userId))
                 .orElseThrow(() -> new UsernameNotFoundException("회원을 찾지 못했습니다."));
 
--- src/main/java/com/freepath/devpath/report/domain/ReportCheck.java
+++ src/main/java/com/freepath/devpath/report/domain/ReportCheck.java
@@ -1,10 +1,18 @@
 package com.freepath.devpath.report.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "REPORT_CHECK")
 public class ReportCheck {
 
@@ -13,8 +21,8 @@ public class ReportCheck {
     private int reportCheckId;
 
     private int reportId;
-    private int adminId;
+    private Integer adminId;    // 기본값 NULL로 설정될 수 있게 Integer로 둠
     private Date checkedAt;
-    private String check_result;
-
+    private String checkResult;
+    private String checkReason;
 }
