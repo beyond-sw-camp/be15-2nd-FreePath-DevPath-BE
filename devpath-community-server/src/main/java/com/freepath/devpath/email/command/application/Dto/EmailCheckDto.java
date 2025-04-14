cfcbfd299b7b9043c10f96ba41d570d7bbae289c
splguyjr <splguyjr@naver.com>
2025-04-15 03:31:20 +0900

    refactor: EmailCheckDto @Schema 의존성 추가

--- devpath-community-server/src/main/java/com/freepath/devpath/email/command/application/Dto/EmailCheckDto.java
+++ devpath-community-server/src/main/java/com/freepath/devpath/email/command/application/Dto/EmailCheckDto.java
@@ -1,5 +1,6 @@
 package com.freepath.devpath.email.command.application.Dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
